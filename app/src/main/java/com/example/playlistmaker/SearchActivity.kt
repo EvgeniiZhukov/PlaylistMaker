890e0f25a87b7399c59df8d1b5bc9ee411b88e25
Evgenii <ev.zhykov@gmail.com>
2023-12-04 22:38:56 +0300

    Готовое домашнее задание для 9ого спринта Первая попытка.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,72 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    var text: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        editText = findViewById(R.id.searchBar)
+
+        val backButton = findViewById<Button>(R.id.buttonBack)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+
+        val clearIcon = findViewById<ImageView>(R.id.clearIcon)
+        clearIcon.setOnClickListener {
+            editText.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearIcon.visibility = clearIconVisibility(p0)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                text = p0.toString()
+            }
+        }
+        editText.addTextChangedListener(simpleTextWatcher)
+    }
+    private fun clearIconVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("text", text)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredText = savedInstanceState.getString("text")
+        editText.setText(restoredText)
     }
 }
